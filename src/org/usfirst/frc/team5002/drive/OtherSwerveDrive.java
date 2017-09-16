be5c560035c90358994e4d15a9b8715b3171a3d3
VegaAce <zvega7596@colliervilledragons.org>
2017-09-16 16:50:05 -0500

    SwerveDrive Constructor

---
+++ src/org/usfirst/frc/team5002/drive/OtherSwerveDrive.java
@@ -0,0 +1,13 @@
+
+
+private WheelDrive backRight;
+private WheelDrive backLeft;
+private WheelDrive frontRight;
+private WheelDrive frontLeft;
+
+public swerveDrive (WheelDrive backLeft, WheelDrive backRight, WheelDrive backLeft, WheelDrive backRight){
+  this.backRight = backRight;
+  this.backLeft = backLeft;
+  this.frontLeft = frontLeft;
+  this.frontRight = frontRight;
+}
