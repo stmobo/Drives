120d7369f8a40cc0ebc64f45d0ac6a76fad337f4
brandon-gong <brandon.gong@brandongong.org>
2017-09-21 18:18:11 -0500

    Start adding teleop command

--- src/org/usfirst/frc/team5002/robot/Robot.java
+++ src/org/usfirst/frc/team5002/robot/Robot.java
@@ -1,36 +1,38 @@
 package org.usfirst.frc.team5002.robot;
 
+import edu.wpi.first.wpilibj.IterativeRobot;
+
+/**
+ * Base class for all robot actions.
+ * TODO: Rewrite. This is all temporary to catch deadlines.
+ *
+ * @author Brandon Gong
+ * Date: 9/21/17
+ */
 public class Robot extends IterativeRobot {
 
     @Override
-    public void robotInit() {
-    }
+    public void robotInit() {}
 
     @Override
-    public void disabledInit() {
-    }
+    public void disabledInit() {}
 
     @Override
-    public void disabledPeriodic() {
-    }
+    public void disabledPeriodic() {}
 
     @Override
-    public void autonomousInit() {
-    }
+    public void autonomousInit() {}
 
     @Override
-    public void autonomousPeriodic() {
-    }
+    public void autonomousPeriodic() {}
 
     @Override
-    public void teleopInit() {
-    }
+    public void teleopInit() {}
 
     @Override
-    public void teleopPeriodic() {
-    }
+    public void teleopPeriodic() {}
 
     @Override
-    public void testPeriodic() {
-    }
+    public void testPeriodic() {}
+
 }
