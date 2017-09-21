51008758b4cf9347a76f2a5dc1f9d6dbfb31cbb0
brandon-gong <brandon.gong@brandongong.org>
2017-09-20 20:40:29 -0500

    add package infos

---
+++ src/org/usfirst/frc/team5002/drive/package-info.java
@@ -0,0 +1,18 @@
+/**
+ * Package {@code drive} contains reusable implementations for drive bases.
+ *
+ * With exception to Drive.java, files should not be placed directly in the
+ * {@code drive} package.  Instead, place it within a subpackage based on the
+ * type of drive the code is implementing.
+ *
+ * For example, the {@code swerve} package contains a SwerveDrive-specific
+ * extension of the Drive interface, and all swerve drive implementations must
+ * implement that interface. This is to ensure that all swerve drive code,
+ * no matter what year or what variety, is compatible with the rest of the
+ * robot code.
+ *
+ * @author Brandon Gong
+ * @author Nikitha Sam
+ * @author Zack Vega
+ */
+package org.usfirst.frc.team5002.drive;
