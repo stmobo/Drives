6a5b6b0036b31381c6659318c0166cd1c0f7ac01
brandon-gong <brandon.gong@brandongong.org>
2017-09-21 18:31:49 -0500

    add robot map from last year

---
+++ src/org/usfirst/frc/team5002/robot/RobotMap.java
@@ -0,0 +1,17 @@
+package org.usfirst.frc.team5002.robot;
+
+public class RobotMap {
+
+    public static int fl_drive = 15;
+    public static int fl_steer = 10;
+
+    public static int fr_drive = 12;
+    public static int fr_steer = 13;
+
+    public static int bl_drive = 19;
+    public static int bl_steer = 1;
+
+    public static int br_drive = 2;
+    public static int br_steer = 4;
+
+}
