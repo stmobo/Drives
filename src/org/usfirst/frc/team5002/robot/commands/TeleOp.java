3430d505b2307096f1b7a1fe6369227d6dc6bb81
brandon-gong <brandon.gong@brandongong.org>
2017-09-21 18:47:32 -0500

    copy in basic methods

--- src/org/usfirst/frc/team5002/robot/commands/TeleOp.java
+++ src/org/usfirst/frc/team5002/robot/commands/TeleOp.java
@@ -5,4 +5,24 @@ import edu.wpi.first.wpilibj.command.Command;
 
 public class TeleOp extends Command {
 
+    public TeleOp() {
+        requires(Robot.swervedrive);
+    }
+
+    protected void initialize() {
+    }
+
+    protected void execute() {
+    }
+
+    protected boolean isFinished() {
+        return false;
+    }
+
+    protected void end() {
+    }
+
+    protected void interrupted() {
+    }
+
 }
