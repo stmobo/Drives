1f34b06ce344d5144063fdd92cc9ec372c2acd2f
brandon-gong <brandon.gong@brandongong.org>
2017-09-21 18:47:10 -0500

    add swervedrive instantiation in init

--- src/org/usfirst/frc/team5002/robot/Robot.java
+++ src/org/usfirst/frc/team5002/robot/Robot.java
@@ -1,6 +1,11 @@
 package org.usfirst.frc.team5002.robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
+import org.usfirst.frc.team5002.drive.swerve.SwerveDrive;
+import org.usfirst.frc.team5002.drive.swerve.SwerveDrive2018;
+import org.usfirst.frc.team5002.drive.swerve.SwerveModule;
+import org.usfirst.frc.team5002.robot.RobotMap;
+import org.usfirst.frc.team5002.robot.commands.TeleOp;
 
 /**
  * Base class for all robot actions.
@@ -11,8 +16,33 @@ import edu.wpi.first.wpilibj.IterativeRobot;
  */
 public class Robot extends IterativeRobot {
 
+    public static SwerveDrive swervedrive;
+
     @Override
-    public void robotInit() {}
+    public void robotInit() {
+        swervedrive = new SwerveDrive2018(
+            new SwerveModule(
+                RobotMap.bl_drive,
+                RobotMap.bl_steer,
+                RobotMap.bl_encoder
+            ),
+            new SwerveModule(
+                RobotMap.br_drive,
+                RobotMap.br_steer,
+                RobotMap.br_encoder
+            ),
+            new SwerveModule(
+                RobotMap.fl_drive,
+                RobotMap.fl_steer,
+                RobotMap.fl_encoder
+            ),
+            new SwerveModule(
+                RobotMap.fr_drive,
+                RobotMap.fr_steer,
+                RobotMap.fr_encoder
+            )
+        );
+    }
 
     @Override
     public void disabledInit() {}
