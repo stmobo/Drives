d7c1bf3ccdff996c875acd035c195ae3e725a1ee
Sebastian Mobo <stmobo@gmail.com>
2017-09-27 14:18:28 -0500

    Remove rough module code

--- src/org/usfirst/frc/team5002/drive/DriveModule.java
+++
@@ -1,9 +0,0 @@
-package org.usfirst.frc.team5002.drive;
-
-public interface DriveModule {
-    public void setReverse(boolean reverseStatus);
-    public void setDrive(float power);
-    public void setDriveSpeed(float speed);
-    public void setDriveDistance(float distance);
-    public boolean isFinished();
-}
